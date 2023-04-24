a08e8012cc3c26d38e05ead483fb9d6bbd9058c7
LeAnhTuan647 <92526269+AnhTuan0011@users.noreply.github.com>
2023-04-24 15:05:32 +0700

    first commit

---
+++ LeAnhTuan0647/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeAnhTuan0647.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
